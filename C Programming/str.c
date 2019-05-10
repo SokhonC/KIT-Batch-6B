5883f17780448cf47fb93ee60d05ab6f215594d0
sokhon <chhunsokhon18@kit.edu.kh>
2019-05-10 07:52:42 +0700

    update

---
+++ C Programming/str.c
@@ -0,0 +1,22 @@
+//
+//  str.c
+//  helloworld
+//
+//  Created by Sokhon Chhun on 4/16/19.
+//  Copyright © 2019 Sokhon Chhun. All rights reserved.
+//
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char name[30] = "Hello";
+    
+    printf("String before strrev() : %s\n",name);
+    
+    //printf("String after strrev( )  : %s",strrev(name));
+    
+    return 0;
+}
+
